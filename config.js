255c4fd70e4b7385fbb6816caae2e4dc148b9038
hwangsangg <hwangsangg@gmail.com>
2022-06-05 22:58:59 +0700

    upload1

---
+++ config.js
@@ -0,0 +1,19 @@
+const CONFIG = {
+    introTitle: 'Gửi Vy',
+    introDesc: `Trái đất vốn lạ thường
+    Mà sao em cứ đi nhầm đường
+    Lạc vào tim anh lẻ loi
+    Đằng sau chữ yêu đây là thương`,
+    btnIntro: 'hihi',
+    title: 'Tao yêu mày nhiều lắm <3',
+    desc: 'Phạm Thuý Vy <3',
+    btnYes: 'Em Yêu Anh <3',
+    btnNo: 'Tao Yêu Mày <3',
+    question:'Thay vì nhập cái gì đó dô đây vì về messenger ib tao i ❤',
+    btnReply: 'Yêu Mày <3',
+    reply: '<3',
+    mess: '<3',
+    messDesc: 'inbox tao i',
+    btnAccept: 'Âu kây :3',
+    messLink: 'https://www.facebook.com/hwangsangg/' //link mess của các bạn. VD: https://m.me/nam.nodemy
+}
